0a6c62b52968a357fac7b96a3e96b6f99bdf7fce
ameniga <gaaloul.b.ameni@gmail.com>
2024-03-27 23:47:33 +0100

    moved delete item to calculator impl

--- pkg/calculator/calculator.go
+++ pkg/calculator/calculator.go
@@ -13,6 +13,7 @@ import (
 
 type Handler interface {
 	AddItemToCart(c *gin.Context)
+	DeleteCartItem(c *gin.Context)
 }
 
 type calculator struct {
@@ -96,6 +97,47 @@ func (cal *calculator) AddItemToCart(c *gin.Context) {
 		}
 	}
 
+	c.Redirect(302, "/")
+}
+
+func (cal *calculator) DeleteCartItem(c *gin.Context) {
+	cartItemIDString := c.Query("cart_item_id")
+	if cartItemIDString == "" {
+		c.Redirect(302, "/")
+		return
+	}
+
+	cookie, _ := c.Request.Cookie("ice_session_id")
+
+	db := cal.db
+
+	var cartEntity entity.CartEntity
+	result := db.Where(fmt.Sprintf("status = '%s' AND session_id = '%s'", entity.CartOpen, cookie.Value)).First(&cartEntity)
+	if result.Error != nil {
+		c.Redirect(302, "/")
+		return
+	}
+
+	if cartEntity.Status == entity.CartClosed {
+		c.Redirect(302, "/")
+		return
+	}
+
+	cartItemID, err := strconv.Atoi(cartItemIDString)
+	if err != nil {
+		c.Redirect(302, "/")
+		return
+	}
+
+	var cartItemEntity entity.CartItem
+
+	result = db.Where(" ID  = ?", cartItemID).First(&cartItemEntity)
+	if result.Error != nil {
+		c.Redirect(302, "/")
+		return
+	}
+
+	db.Delete(&cartItemEntity)
 	c.Redirect(302, "/")
 }
 
