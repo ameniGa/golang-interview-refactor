8b846b0e150ba949a8c156df8048a09ae95919fc
ameniga <gaaloul.b.ameni@gmail.com>
2024-03-28 14:01:17 +0100

    updated controller initialization in main

--- cmd/web-api/main.go
+++ cmd/web-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"interview/pkg/calculator"
 	"interview/pkg/controllers"
 	"interview/pkg/db"
 	"net/http"
@@ -11,8 +12,13 @@ func main() {
 	db.MigrateDatabase()
 
 	ginEngine := gin.Default()
-
-	var taxController controllers.TaxController
+	itemPriceMapping := map[string]float64{
+		"shoe":  100,
+		"purse": 200,
+		"bag":   300,
+		"watch": 300,
+	}
+	taxController := controllers.NewTaxController(calculator.NewCalculator(itemPriceMapping, db.GetDatabase()))
 	ginEngine.GET("/", taxController.ShowAddItemForm)
 	ginEngine.POST("/add-item", taxController.AddItem)
 	ginEngine.GET("/remove-cart-item", taxController.DeleteCartItem)
